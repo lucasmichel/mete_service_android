36f670fb0c9195e22132b86012685625beca318c
Pedro de Sousa Santa Rosa <pedronks@hotmail.com>
2013-03-11 15:34:16 -0300

    oii

--- Meeting_Service/src/com/example/meeting_service/ListarAcompanhanteActivity.java
+++ Meeting_Service/src/com/example/meeting_service/ListarAcompanhanteActivity.java
@@ -9,6 +9,7 @@ protected void onCreate(Bundle savedInstanceState) {
 	// TODO Auto-generated method stub
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_main);
+	//oi
 }
 }
 
