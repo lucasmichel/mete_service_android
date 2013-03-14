27538ce2167fc1fcb1013019ee8b6993be06d908
Marcio <mrcioholanda@gmail.com>
2013-03-14 13:58:46 -0300

    Correções.

--- Meeting_Service/src/mete_service/model/TelaAcompanhanteActivity.java
+++ Meeting_Service/src/mete_service/model/TelaAcompanhanteActivity.java
@@ -1,9 +1,7 @@
 package mete_service.model;
 
-import edu.unibratec.banco.R;
 import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
+
 
 public class TelaAcompanhanteActivity extends Activity {
 
