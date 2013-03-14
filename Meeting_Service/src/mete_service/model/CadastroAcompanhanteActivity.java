21b77cc496b56b3a9f73ebc84bbcaaa3ea7c547a
Marcio <mrcioholanda@gmail.com>
2013-03-14 13:56:16 -0300

    Telas de Cadastro de acompanhante e tela de gerenciamento de perfil criadas[Interface Gráfica].

---
+++ Meeting_Service/src/mete_service/model/CadastroAcompanhanteActivity.java
@@ -0,0 +1,8 @@
+package mete_service.model;
+
+import android.app.Activity;
+
+public class CadastroAcompanhanteActivity extends Activity {
+
+}
+
---
+++ Meeting_Service/src/mete_service/model/TelaAcompanhanteActivity.java
@@ -0,0 +1,11 @@
+package mete_service.model;
+
+import edu.unibratec.banco.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TelaAcompanhanteActivity extends Activity {
+
+	 
+}
