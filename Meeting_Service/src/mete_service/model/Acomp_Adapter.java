5d124d92f7a70bf0d0464f7e09ccc97bc9426122
Pedro de Sousa Santa Rosa <pedronks@hotmail.com>
2013-03-13 21:49:57 -0300

    Adpater de acompanhanhte

--- Meeting_Service/src/mete_service/model/Acomp_Adapter.java
+++ Meeting_Service/src/mete_service/model/Acomp_Adapter.java
@@ -2,13 +2,17 @@ package mete_service.model;
 
 import java.util.List;
 
+import com.example.meeting_service.R;
+
+
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-public class Acomp_Adapter extends ArrayAdapter {
+public class Acomp_Adapter extends ArrayAdapter<Acompanhante> {
 	
 	private List<Acompanhante> listaAcompanhante;
 	
@@ -27,7 +31,15 @@ public class Acomp_Adapter extends ArrayAdapter {
 	}
 	
 	public View getView(int position, View convertView, ViewGroup parent) {
-		return null;
+		Acompanhante acompanhante = getItem(position);
+		
+		convertView = LayoutInflater.from(getContext()).inflate(
+				R.layout.linha_acomp,null);
+		
+		
+		
+		
+		return convertView;
 		
 	}
 
