f3f9d2a4da63f5897d3f96cd684567aed4731300
sostenessoares <sostenessoares@hotmail.com>
2013-03-30 16:28:31 -0300

    Model Encontro

---
+++ Meeting_Service/src/br/uni/mete_service/model/Encontro.java
@@ -0,0 +1,60 @@
+package br.uni.mete_service.model;
+
+import android.provider.ContactsContract.Data;
+import br.uni.mete_service.model.repositorio.ModelClass;
+
+public class Encontro extends ModelClass {
+
+	private String id;
+	private String cliente_id;
+	private String data;
+	private String hora;
+	
+	public Encontro() {
+		super();
+	}
+	
+	public Encontro(String id, String cliente_id, String data, String hora,
+			int status, String mensagem) {
+		super();
+		this.id = id;
+		this.cliente_id = cliente_id;
+		this.data = data;
+		this.hora = hora;
+		this.setMensagem(mensagem);
+		this.setStatus(status);
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCliente_id() {
+		return cliente_id;
+	}
+
+	public void setCliente_id(String cliente_id) {
+		this.cliente_id = cliente_id;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	
+}
