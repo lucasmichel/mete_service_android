28207b84c30c2e818921f0a9fc1e58ec63e7ce1f
Pedro de Sousa Santa Rosa <pedronks@hotmail.com>
2013-04-10 21:29:56 -0300

    marcio

---
+++ Mette_Service_2.3/src/br/uni/mette_service/Controller/Avaliacao/AvaliacaoAcompanhante.java
@@ -0,0 +1,10 @@
+package br.uni.mette_service.Controller.Avaliacao;
+
+import br.uni.mette_service.Model.Acompanhante;
+import br.uni.mette_service.Model.Cliente;
+
+public class AvaliacaoAcompanhante {
+private int nota;
+private Acompanhante acompanhante;
+private Cliente cliente;
+}
