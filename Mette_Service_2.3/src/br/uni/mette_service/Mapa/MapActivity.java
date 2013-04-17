a4778bdcea19cc95c0084fd2a3c7aafef51a2900
Pedro de Sousa Santa Rosa <pedronks@hotmail.com>
2013-04-17 20:22:14 -0300

    Pin no mapa, com titulo e descrição

--- Mette_Service_2.3/src/br/uni/mette_service/Mapa/MapActivity.java
+++ Mette_Service_2.3/src/br/uni/mette_service/Mapa/MapActivity.java
@@ -14,85 +14,101 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity 
 implements LocationListener {  
 
-private GoogleMap googleMap;
-
-protected void onCreate(Bundle savedInstanceState) {  
-super.onCreate(savedInstanceState);  
-setContentView(R.layout.mapa); 
-setUpMap();
-
-}
-
-@Override
-public boolean onCreateOptionsMenu(Menu menu) {		
-	getMenuInflater().inflate(R.menu.main, menu);
-	return true;
-}
-
-public void onLocationChanged(Location location) {
-LatLng latLog	= new LatLng(location.getLatitude(), location.getLongitude());
-
-googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLog));
-googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-}
-
-public void onProviderDisabled(String arg0) {
-// TODO Auto-generated method stub
-
-}
-
-public void onProviderEnabled(String arg0) {
-// TODO Auto-generated method stub
-
-}
-
-public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-// TODO Auto-generated method stub
-
-}
-
-private void setUpMap () {
-if (googleMap == null){
-googleMap = ((SupportMapFragment)getSupportFragmentManager()
-		.findFragmentById(R.id.map)).getMap();
-
-	if (googleMap != null){
-		googleMap.setMyLocationEnabled(true);
-		
-		LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-		
-		String provider = lm.getBestProvider(new Criteria(), true);
-		
-		if (provider == null) {
-			onProviderDisabled(provider);
-		}
-		Location loc = lm.getLastKnownLocation(provider);
-		
-		if (loc != null) {
-			onLocationChanged(loc);
-		}
-		
-		googleMap.setOnMapLongClickListener(onLongClick());
-	}
-}
-}
-
-private OnMapLongClickListener onLongClick() {
-// TODO Auto-generated method stub
-return new OnMapLongClickListener() {
+	private GoogleMap googleMap;
 	
-	public void onMapLongClick(LatLng point) {
-		// TODO Auto-generated method stub
-		System.out.println(point.toString());
-		Toast.makeText(MapActivity.this, point.toString(), Toast.LENGTH_LONG).show();
+	protected void onCreate(Bundle savedInstanceState) {  
+	super.onCreate(savedInstanceState);  
+	setContentView(R.layout.mapa); 
+	setUpMap();
+	
 	}
-};
-}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {		
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void onLocationChanged(Location location) {
+//	LatLng latLog	= new LatLng(-8.05668, -34.896011);
+		
+		//------
+		// Onde latLog recebe os valores do serviço da acompanhante
+		//------
+	LatLng latLog	= new LatLng(location.getLatitude(), location.getLongitude());
+	
+	googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLog));
+	googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+	
+	googleMap.addMarker(new MarkerOptions()  
+    .position(latLog)  
+    .icon(BitmapDescriptorFactory.fromResource(  
+      R.drawable.pin)).
+      title("QUALQUER LUGAR")  
+      .snippet("PERNAMBUCO"));
+	
+	
+	}
+	
+	public void onProviderDisabled(String arg0) {
+	// TODO Auto-generated method stub
+	
+	}
+	
+	public void onProviderEnabled(String arg0) {
+	// TODO Auto-generated method stub
+	
+	}
+	
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+	// TODO Auto-generated method stub
+	
+	}
+	
+	private void setUpMap () {
+	if (googleMap == null){
+	googleMap = ((SupportMapFragment)getSupportFragmentManager()
+			.findFragmentById(R.id.map)).getMap();
+	
+		if (googleMap != null){
+			googleMap.setMyLocationEnabled(true);
+			
+			LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+			
+			String provider = lm.getBestProvider(new Criteria(), true);
+			
+			if (provider == null) {
+				onProviderDisabled(provider);
+			}
+			Location loc = lm.getLastKnownLocation(provider);
+			
+			if (loc != null) {
+				onLocationChanged(loc);
+			}
+			
+			googleMap.setOnMapLongClickListener(onLongClick());
+		}
+	}
+	}
+	
+	private OnMapLongClickListener onLongClick() {
+	// TODO Auto-generated method stub
+	return new OnMapLongClickListener() {
+		
+		public void onMapLongClick(LatLng point) {
+			// TODO Auto-generated method stub
+			System.out.println(point.toString());
+			Toast.makeText(MapActivity.this, point.toString(), Toast.LENGTH_LONG).show();
+		}
+	};
+	}
 
 
 }
