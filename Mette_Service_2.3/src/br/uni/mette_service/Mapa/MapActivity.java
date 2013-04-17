11eaa1aac257b335eac7cfba9bed8a983454f7eb
Pedro de Sousa Santa Rosa <pedronks@hotmail.com>
2013-04-17 19:42:36 -0300

    mapas

---
+++ Mette_Service_2.3/src/br/uni/mette_service/Mapa/MapActivity.java
@@ -0,0 +1,98 @@
+package br.uni.mette_service.Mapa;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.widget.Toast;
+import br.uni.mette_service.R;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MapActivity extends FragmentActivity 
+implements LocationListener {  
+
+private GoogleMap googleMap;
+
+protected void onCreate(Bundle savedInstanceState) {  
+super.onCreate(savedInstanceState);  
+setContentView(R.layout.mapa); 
+setUpMap();
+
+}
+
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {		
+	getMenuInflater().inflate(R.menu.main, menu);
+	return true;
+}
+
+public void onLocationChanged(Location location) {
+LatLng latLog	= new LatLng(location.getLatitude(), location.getLongitude());
+
+googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLog));
+googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+}
+
+public void onProviderDisabled(String arg0) {
+// TODO Auto-generated method stub
+
+}
+
+public void onProviderEnabled(String arg0) {
+// TODO Auto-generated method stub
+
+}
+
+public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+// TODO Auto-generated method stub
+
+}
+
+private void setUpMap () {
+if (googleMap == null){
+googleMap = ((SupportMapFragment)getSupportFragmentManager()
+		.findFragmentById(R.id.map)).getMap();
+
+	if (googleMap != null){
+		googleMap.setMyLocationEnabled(true);
+		
+		LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+		
+		String provider = lm.getBestProvider(new Criteria(), true);
+		
+		if (provider == null) {
+			onProviderDisabled(provider);
+		}
+		Location loc = lm.getLastKnownLocation(provider);
+		
+		if (loc != null) {
+			onLocationChanged(loc);
+		}
+		
+		googleMap.setOnMapLongClickListener(onLongClick());
+	}
+}
+}
+
+private OnMapLongClickListener onLongClick() {
+// TODO Auto-generated method stub
+return new OnMapLongClickListener() {
+	
+	public void onMapLongClick(LatLng point) {
+		// TODO Auto-generated method stub
+		System.out.println(point.toString());
+		Toast.makeText(MapActivity.this, point.toString(), Toast.LENGTH_LONG).show();
+	}
+};
+}
+
+
+}
